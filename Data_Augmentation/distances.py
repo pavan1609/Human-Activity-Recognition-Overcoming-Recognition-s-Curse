b334eb5fa813601230497a7f2beff563702a7d35
Pavankumar Laxman Chandankar <69724969+pavan1609@users.noreply.github.com>
2024-07-04 15:04:20 +0200

    Update distances.py

--- Data_Augmentation/distances.py
+++ Data_Augmentation/distances.py
@@ -1,21 +1,22 @@
 import numpy as np
+from numba import jit
 
+@jit(nopython=True)
 def dynamic_time_warping(x, y):
-    n = x.shape[0]
-    m = y.shape[0]
+    n, m = x.shape[0], y.shape[0]
     cost = np.zeros((n, m))
 
-    cost[0, 0] = np.abs(x[0] - y[0])
+    cost[0, 0] = np.linalg.norm(x[0] - y[0])
 
     for i in range(1, n):
-        cost[i, 0] = cost[i-1, 0] + np.abs(x[i] - y[0])
+        cost[i, 0] = cost[i-1, 0] + np.linalg.norm(x[i] - y[0])
 
     for j in range(1, m):
-        cost[0, j] = cost[0, j-1] + np.abs(x[0] - y[j])
+        cost[0, j] = cost[0, j-1] + np.linalg.norm(x[0] - y[j])
 
     for i in range(1, n):
         for j in range(1, m):
-            minimum_cost = min(cost[i-1, j], cost[i, j-1], cost[i-1, j-1])
-            cost[i, j] = np.abs(x[i] - y[j]) + minimum_cost
+            min_cost = min(cost[i-1, j], cost[i, j-1], cost[i-1, j-1])
+            cost[i, j] = np.linalg.norm(x[i] - y[j]) + min_cost
 
-    return cost[-1, -1]
+    return cost[-1, -1], (n, m)  # Returning path size as a placeholder, modify as needed
