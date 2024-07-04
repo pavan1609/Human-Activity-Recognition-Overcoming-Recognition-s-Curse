b72800d590337087c4888edaf6d9c8594ac65670
Pavankumar Laxman Chandankar <69724969+pavan1609@users.noreply.github.com>
2024-07-04 15:04:50 +0200

    Update knn.py

--- Data_Augmentation/knn.py
+++ Data_Augmentation/knn.py
@@ -1,10 +1,13 @@
-import numpy as np
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score
 
 class KNNClassifier:
-    def __init__(self, n_neighbors=5):
-        self.model = KNeighborsClassifier(n_neighbors=n_neighbors)
+    def __init__(self, n_neighbors=5, algorithm='auto', leaf_size=30, n_jobs=-1):
+        self.model = KNeighborsClassifier(n_neighbors=n_neighbors, algorithm=algorithm, leaf_size=leaf_size, n_jobs=n_jobs)
 
     def fit(self, x_train, y_train, x_test, y_test):
         self.model.fit(x_train, y_train)
-        return self.model.predict(x_test)
+        y_pred = self.model.predict(x_test)
+        accuracy = accuracy_score(y_test, y_pred)
+        print(f"KNN classifier accuracy: {accuracy}")
+        return y_pred
